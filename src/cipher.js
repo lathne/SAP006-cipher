ac71dbd122007471e68df3dd601018962f40f63e
lathne <laurathomazi@gmail.com>
2021-06-02 23:07:41 -0300

    Inicio da Estrutura HTML e cipher.js

---
+++ src/cipher.js
@@ -0,0 +1,25 @@
+// aqui você deve implementar o objeto cipher, o qual já está exportado no boilerplate. Este objeto (cipher) deve conter dois métodos:
+
+//cipher.encode(offset, string): offset é o número de posições que queremos mover para a direita no alfabeto e string é a mensagem (texto) que queremos cifrar.
+
+//cipher.decode(offset, string): offset é o número de posições que queremos mover para a esquerda no alfabeto e string é a mensagem (texto) que queremos decifrar.
+
+
+const cipher = {
+    
+ encode: function(){
+
+ }
+
+//  decode: function(){
+
+//  }
+
+};
+  
+export default cipher;
+
+
+
+
+// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//aqui você deve escutar os eventos de DOM, chamar cipher.encode() e cipher.decode().
+
+
+import cipher from './cipher.js';
+
+console.log(cipher);
