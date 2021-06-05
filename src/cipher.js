16f86735cd9f7929b700de30f30877559f44f0af
lathne <laurathomazi@gmail.com>
2021-06-04 21:17:08 -0300

    Iniciando o index.js e Style.css

--- src/cipher.js
+++ src/cipher.js
@@ -2,24 +2,27 @@
 
 const cipher = {
     
- encode: function(offset, string){
+//  encode: function(offset, string){
      
-    let cipherText = "";
-        //for loop para obter o valor unicode de cada index da string
-    for(i=0; i<=string.lenght; i++) {
-        //Letra A na tabela ASCII tem valor 65, letra Z tem valor 90
-        if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-            //formula do Daniel 
-           let newString = string.charCodeAt(i) - 65 + offset %26 + 65;
-        }
-        cipherText+=String.fromCharCode(newString);
-    }
-    return cipherText;
- }
-
-//  decode: function(){
-
+//     // let caesarCode = "";
+//     // let undefinedText = "";
+//     let cipherText = "";
+//     let newString = "";
+//         //for loop para obter o valor unicode de cada index da string
+//     for(let= i=0; i<=string.length; i++) {
+//         //Letra A na tabela tem valor 65, letra Z tem valor 90
+//         if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+//             //formula do Daniel 
+//            newString = string.charCodeAt(i) - 65 + offset %26 + 65;
+//         }
+//         cipherText = String.fromCharCode(newString);  
+//     }
+//     return cipherText;     
 //  }
+
+// //  decode: function(){
+
+// //  }
 
 };
   
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-//aqui você deve escutar os eventos de DOM, chamar cipher.encode() e cipher.decode().
 
 
 import cipher from './cipher.js';
+
+let encodeButton = document.getElementById('encode');
+let decodeButton = document.getElementById('decode');
+let result = document.getElementById('result');
+
+encodeButton.addEventListener("click", e => {
+    result.innerHTML = cipher.encode();
+})
+
+decodeButton.addEventListener("click", e => {
+    result.innerHTML = cipher.decode();
+})
+
+
+
+/* 
+let key = document.getElementById('key').value ??
+let textArea = document.getElementById('textarea').value ??
+*/
 
 console.log(cipher);
