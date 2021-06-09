c4b23afd5dc02c6baf25ac73f220bd2db095854d
lathne <laurathomazi@gmail.com>
2021-06-08 22:12:05 -0300

    Correçao no cipher.js, finalizaçao do index.js e continuando estilizaçao CSS

--- src/cipher.js
+++ src/cipher.js
@@ -2,32 +2,37 @@ const cipher = {
     
  encode: function(offset, string){
      let cipherEncode = "";
-     let caesarCode = "";
-    
+     let offsetNumber = parseInt(offset);
+     
      for(let i=0; i<=string.length; i++) {
         if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-            caesarCode = ((string.charcodeAt(i) - 65 + offset) % 26) + 65;
+           let caesarCode = ((string.charCodeAt(i) - 65 + offsetNumber) % 26) + 65;
+           cipherEncode += String.fromCharCode(caesarCode);     
         } 
         else if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-            caesarCode = ((string.charcodeAt(i) - 97 + offset) % 26) + 97;
-        }
-        cipherText = String.fromCharCode(caesarCode);  
+            let caesarCode = ((string.charCodeAt(i) - 97 + offsetNumber) % 26) + 97;
+            cipherEncode += String.fromCharCode(caesarCode); 
+        }    
     }
-    return cipherEncode;
+   console.log(cipherEncode);
+   return cipherEncode;
  },
 
  decode: function(offset, string){
     let cipherDecode = "";
-    let caesarCode = "";
+    let offsetNumber = parseInt(offset);
+     
     for(let i=0; i<=string.length; i++) {
-        if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-            caesarCode = ((string.charcodeAt(i) - 65 + offset) % 26);
-        } 
-        else if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
-            caesarCode = ((string.charcodeAt(i) - 97 + offset) % 26);
-        }
-        cipherText = String.fromCharCode(caesarCode);  
+       if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          let caesarCode = 90-((90 - string.charCodeAt(i) + offsetNumber) % 26);
+          cipherDecode += String.fromCharCode(caesarCode);  
+       } 
+       else if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+           let caesarCode = 122-((122 - string.charCodeAt(i) + offsetNumber) % 26);
+           cipherDecode += String.fromCharCode(caesarCode); 
+       }
     }
+    console.log(cipherDecode);
     return cipherDecode;
  }
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+    
 
 const encodeButton = document.getElementById('encodeButton');
+
 //addEventListener > Event type, function call back
-encodeButton.addEventListener("click", e => { 
+encodeButton.addEventListener("click", event => { 
+    event.preventDefault()
     let offset = document.getElementById("key").value;
     let string = document.getElementById("textarea").value;
+    document.getElementById('result').innerHTML = cipher.encode(offset, string);
 })
-document.getElementById(result).innerHtml = cipher.encode(offset, string);
+
+
 
 
 const decodeButton = document.getElementById('decodeButton');
 
-decodeButton.addEventListener("click", e => { 
+decodeButton.addEventListener("click", event => { 
+    event.preventDefault()
     let offset = document.getElementById("key").value;
     let string = document.getElementById("textarea").value;
-    
+    document.getElementById('result').innerHTML = cipher.decode(offset, string);
 })
-document.getElementById(result).innerHtml = cipher.decode(offset, string);
-
 
 
