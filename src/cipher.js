ecccf92dfc328da34088265d104dea8e829bd540
lathne <laurathomazi@gmail.com>
2021-06-16 18:05:15 -0300

    remoçao de console.log e da maioria dos comentarios

--- src/cipher.js
+++ src/cipher.js
@@ -11,31 +11,30 @@ const cipher = {
      
      for(let i=0; i<=string.length; i++) {
         let charCode = string.charCodeAt(i);
-            //numeros
+          
          if(charCode >= 48 && charCode <= 57) {
             let switchCode = ((charCode - 48 + offsetNumber) % 10) + 48;
             cipherEncode += String.fromCharCode(switchCode); 
-         } //letras maiusculas
+         } 
          else if(charCode >= 65 && charCode <= 90) {
             let switchCode = ((charCode - 65 + offsetNumber) % 26) + 65;
             cipherEncode += String.fromCharCode(switchCode);     
-         } //letras minusculas
+         } 
          else if(charCode >= 97 && charCode <= 122) {
             let switchCode = ((charCode - 97 + offsetNumber) % 26) + 97;
             cipherEncode += String.fromCharCode(switchCode); 
-         } //espaço, ! e @
+         } 
          else if(charCode == 32 || charCode == 33 || charCode == 64) {
-           //retorne o caractere nesse index específico 
+           
             cipherEncode += string.charAt(i);
-         } //sinais e pontuação " # $ % & ' () * + , - . /
+         }
          else if(charCode >= 34 && charCode <= 47) {
             cipherEncode += string.charAt(i);
-         } //sinais e pontuação : ; < = > ?
+         } 
          else if(charCode >= 58 && charCode <= 63) {
             cipherEncode += string.charAt(i);
          }
      }
-   console.log(cipherEncode);
    return cipherEncode;
  },
 
@@ -72,7 +71,6 @@ const cipher = {
          cipherDecode += string.charAt(i);
       }      
     }
-    console.log(cipherDecode);
     return cipherDecode;
  }
 
